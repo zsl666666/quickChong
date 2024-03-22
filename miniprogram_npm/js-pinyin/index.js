c0b333df76ae96bea6cf9941d072822ccc0685ba
zhoushanlin <zhoushanlin@inke.cn>
2024-03-22 16:11:40 +0800

    feat: 设备详情页开发

---
+++ miniprogram_npm/js-pinyin/index.js
@@ -0,0 +1,17 @@
+/**
+ * @fileOverview 导出默认的实例
+ * @author waterchestnut 2017/3/13
+ * @module
+ */
+'use strict';
+
+let pinyin = new (require('./dist/pinyin'))({charCase:0});
+/*pinyin.setOptions({checkPolyphone: true, charCase: 0});
+console.log(pinyin.getFullChars('管理员'))
+console.log(pinyin.getCamelChars('管理员'))
+console.log(pinyin.getCamelChars('1234'))
+console.log(pinyin.getCamelChars('english'))
+console.log(pinyin.getCamelChars('昕'))
+console.log(pinyin.getCamelChars('佛'))
+console.log(pinyin.getFullChars('佛'))*/
+module.exports = pinyin;
--- pages/deviceDetail/index.js
+++ pages/deviceDetail/index.js
@@ -9,6 +9,9 @@ Page({
    */
   data: {
     navBarHeight: app.globalData.navBarHeight,
+    // 设备图片
+    pictures: ['https://gathertrack.oss-cn-beijing.aliyuncs.com/2024/03/JvRXkeKU1jr0bO9o2ZfMTQCcxqyIhBPS.jpg?x-oss-process=image/resize,w_200,h_160', 'https://gathertrack.oss-cn-beijing.aliyuncs.com/2024/03/TtojLp9vQ3cM17miEydAWkGxZqKNwVIl.jpg?x-oss-process=image/resize,w_200,h_160', 'https://gathertrack.oss-cn-beijing.aliyuncs.com/2024/03/C4OwPZe9J5yND6pTIndaLkrjsuU8AmWg.png?x-oss-process=image/resize,w_200,h_160'],
+    currentImageIndex: 0, // 当前swiper所在滑块的 index
   },
 
   /**
@@ -72,4 +75,31 @@ Page({
       url: '/pages/index/index',
     })
   },
+
+  // 预览详情大图
+  handlePreview(e) {
+    const curUrl = e.currentTarget.dataset.item
+    wx.previewImage({
+      current: curUrl,
+      urls: this.data.pictures || [],
+    })
+  },
+
+  // current 改变时会触发 change 事件
+  handleSwiperChange(event) {
+    this.setData({
+      currentImageIndex: event.detail.current
+    })
+  },
+
+  // 点击缩略图
+  handleClickThum(e) {
+    this.setData({
+      currentImageIndex: e.currentTarget.dataset.index
+    })
+  },
+
+  handleTransition(e) {
+    // console.log('fffsfdsfsfs', e)
+  }
 })
